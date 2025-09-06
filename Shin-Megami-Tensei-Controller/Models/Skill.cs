979e8abbaaad34c150a707bd11f147a5f507788c
Victor Ruiz <veruiz@uc.cl>
2025-09-05 22:29:38 -0400

    feat(Models/Skill): agregar clase Skill para gestionar habilidades en el juego

---
+++ Shin-Megami-Tensei-Controller/Models/Skill.cs
@@ -0,0 +1,28 @@
+﻿namespace Shin_Megami_Tensei.Models;
+
+public class Skill
+{
+    public string Name { get; set; } = "";
+    public string Type { get; set; } = "";
+    public int Cost { get; set; }
+    public int Power { get; set; }
+    public string Target { get; set; } = "";
+    public string Hits { get; set; } = "";
+    public string Effect { get; set; } = "";
+
+    public Skill(string name, string type, int cost, int power, string target, string hits, string effect)
+    {
+        Name = name;
+        Type = type;
+        Cost = cost;
+        Power = power;
+        Target = target;
+        Hits = hits;
+        Effect = effect;
+    }
+
+    public bool CanBeUsedBy(Unit unit)
+    {
+        return unit.CurrentMP >= Cost;
+    }
+}
