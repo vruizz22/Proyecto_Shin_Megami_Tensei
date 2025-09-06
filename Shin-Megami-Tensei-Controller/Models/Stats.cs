2d1cd19d273b2732293b117040ab72b7083a7759
Victor Ruiz <veruiz@uc.cl>
2025-09-05 22:29:43 -0400

    feat(Models/Stats): agregar clase Stats para gestionar estadísticas de personajes

---
+++ Shin-Megami-Tensei-Controller/Models/Stats.cs
@@ -0,0 +1,23 @@
+﻿namespace Shin_Megami_Tensei.Models;
+
+public class Stats
+{
+    public int HP { get; set; }
+    public int MP { get; set; }
+    public int Str { get; set; }
+    public int Skl { get; set; }
+    public int Mag { get; set; }
+    public int Spd { get; set; }
+    public int Lck { get; set; }
+
+    public Stats(int hp, int mp, int str, int skl, int mag, int spd, int lck)
+    {
+        HP = hp;
+        MP = mp;
+        Str = str;
+        Skl = skl;
+        Mag = mag;
+        Spd = spd;
+        Lck = lck;
+    }
+}
