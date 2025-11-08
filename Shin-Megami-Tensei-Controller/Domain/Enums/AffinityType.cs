b1b12f6da014efe286fd24445065379221980277
VICTOR RUIZ <veruiz@uc.cl>
2025-11-08 06:41:53 -0300

    feat(Enums): agregar clases AffinityType y ElementType con sus extensiones para gestionar tipos de afinidad y elementos

---
+++ Shin-Megami-Tensei-Controller/Domain/Enums/AffinityType.cs
@@ -0,0 +1,41 @@
+﻿namespace Shin_Megami_Tensei.Domain.Enums;
+
+public enum AffinityType
+{
+    Neutral,  // "-"
+    Weak,     // "Wk"
+    Resist,   // "Rs"
+    Null,     // "Nu"
+    Repel,    // "Rp"
+    Drain     // "Dr"
+}
+
+public static class AffinityTypeExtensions
+{
+    public static AffinityType FromString(string affinity)
+    {
+        return affinity switch
+        {
+            "Wk" => AffinityType.Weak,
+            "Rs" => AffinityType.Resist,
+            "Nu" => AffinityType.Null,
+            "Rp" => AffinityType.Repel,
+            "Dr" => AffinityType.Drain,
+            _ => AffinityType.Neutral
+        };
+    }
+
+    public static string ToDisplayString(this AffinityType type)
+    {
+        return type switch
+        {
+            AffinityType.Weak => "Wk",
+            AffinityType.Resist => "Rs",
+            AffinityType.Null => "Nu",
+            AffinityType.Repel => "Rp",
+            AffinityType.Drain => "Dr",
+            _ => "-"
+        };
+    }
+}
+
---
+++ Shin-Megami-Tensei-Controller/Domain/Enums/ElementType.cs
@@ -0,0 +1,57 @@
+﻿namespace Shin_Megami_Tensei.Domain.Enums;
+
+public enum ElementType
+{
+    Physical,
+    Gun,
+    Fire,
+    Ice,
+    Electric,
+    Force,
+    Light,
+    Dark,
+    Almighty
+}
+
+public static class ElementTypeExtensions
+{
+    public static ElementType FromString(string element)
+    {
+        return element switch
+        {
+            "Phys" => ElementType.Physical,
+            "Gun" => ElementType.Gun,
+            "Fire" => ElementType.Fire,
+            "Ice" => ElementType.Ice,
+            "Elec" => ElementType.Electric,
+            "Force" => ElementType.Force,
+            "Light" => ElementType.Light,
+            "Dark" => ElementType.Dark,
+            "Almighty" => ElementType.Almighty,
+            _ => ElementType.Physical
+        };
+    }
+
+    public static string ToGameString(this ElementType element)
+    {
+        return element switch
+        {
+            ElementType.Physical => "Phys",
+            ElementType.Gun => "Gun",
+            ElementType.Fire => "Fire",
+            ElementType.Ice => "Ice",
+            ElementType.Electric => "Elec",
+            ElementType.Force => "Force",
+            ElementType.Light => "Light",
+            ElementType.Dark => "Dark",
+            ElementType.Almighty => "Almighty",
+            _ => "Phys"
+        };
+    }
+
+    public static bool IsInstantKillElement(this ElementType element)
+    {
+        return element == ElementType.Light || element == ElementType.Dark;
+    }
+}
+
